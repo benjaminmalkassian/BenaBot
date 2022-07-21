517c22195df57fd05fe407b2c6a3e2f4d57bdc2e
bmalkassian <benjamin.malkassian@soprasteria.com>
2022-07-21 15:11:32 +0200

    Suppression of discord Token in main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from BenaBotClient import BenaBotClient
 config = configparser.ConfigParser()
 config.read("config.ini")
 DISCORD_TOKEN = config['DEFAULT']['discord_token']
-id_channel = "999646444925489232"
-
 
 if __name__ == "__main__":
     bot = BenaBotClient()
-    bot.run("OTk5NjQ3MTQ3Nzc3NTk3NTMy.GX3Mz7.1Nxao3oIdwJtrCoTOivLLRf6xlJFC-17KJq8NM")
+    bot.run("DISCORD_TOKEN")
